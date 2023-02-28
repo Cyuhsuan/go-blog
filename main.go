7f2ed11bd5795a01f18c0fdaebd1800b707d58da
yuHsuan <x89301089@gmail.com>
2023-02-28 13:24:57 +0800

    [add] config loading

--- main.go
+++ main.go
@@ -3,17 +3,32 @@ package main
 import (
 	"go-blog/app/controller/authcontroller"
 	"go-blog/app/middleware"
+	"go-blog/config"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// loaing config
+	config := appInit()
 	router := gin.Default()
+	// 註冊
 	router.POST("/register", authcontroller.Register)
+	// 登入
 	router.POST("/login", authcontroller.Login)
+	// 登入權限 middleware
 	router.Use(middleware.Auth)
 	{
 		router.POST("/logout", authcontroller.Logout)
 	}
-	router.Run(":8080")
+
+	router.Run(":" + config.Port)
+}
+
+func appInit() config.Config {
+	conf, err := config.LoadConfig()
+	if err != nil {
+		panic("config loading error")
+	}
+	return conf
 }
